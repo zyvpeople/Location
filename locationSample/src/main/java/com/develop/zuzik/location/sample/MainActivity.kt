1490ec41638af52c14559b4ef66bf0742f6e563c
Yaroslav Zozulia <zyvpeople@gmail.com>
2017-01-07 13:57:29 +0200

    refactored sample

--- locationSample/src/main/java/com/develop/zuzik/location/sample/MainActivity.kt
+++ locationSample/src/main/java/com/develop/zuzik/location/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.develop.zuzik.location.sample
 import android.location.Location
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.develop.zuzik.location.core.location_provider.LocationProvider
 import com.develop.zuzik.location.googleplayservice.location_provider.GooglePlayServicesLocationProvider
 import com.develop.zuzik.location.manager.location_provider.LocationManagerLocationProvider
 import com.develop.zuzik.locationmanager.R
@@ -18,12 +19,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val locationManagerLocationProvider = LocationManagerLocationProvider(this, 10000, 10f)
-        val googlePlayServiceLocationProvider = GooglePlayServicesLocationProvider(this, buildLocationRequest())
 
-        val askLocation = RxView
-                .clicks(getLocation)
-        val locationProvider = RxCompoundButton
+        askLocation()
+                .withLatestFrom(locationProvider()) { askLocation, locationProvider -> locationProvider }
+                .flatMap {
+                    it.asObservable()
+                            .compose(toTryMonad())
+                            .take(1)
+                }
+                .map(monadToString())
+                .subscribe(RxTextView.text(location))
+    }
+
+    private fun createGooglePlayServicesLocationProvider() = GooglePlayServicesLocationProvider(this, createLocationRequest())
+
+    private fun createLocationRequest() =
+            LocationRequest()
+                    .setInterval(10000)
+                    .setFastestInterval(5000)
+                    .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+
+    private fun createLocationManagerLocationProvider() = LocationManagerLocationProvider(this, 10000, 10f)
+
+    private fun askLocation() = RxView.clicks(getLocation)
+
+    private fun locationProvider(): Observable<LocationProvider>? {
+        val googlePlayServiceLocationProvider = createGooglePlayServicesLocationProvider()
+        val locationManagerLocationProvider = createLocationManagerLocationProvider()
+        return RxCompoundButton
                 .checkedChanges(locationProviderSwitch)
                 .defaultIfEmpty(false)
                 .map { checked ->
@@ -33,37 +56,21 @@ class MainActivity : AppCompatActivity() {
                         locationManagerLocationProvider
                     }
                 }
-        val toTryMonad: (Observable<Location>) -> Observable<TryMonad<Location>> = {
-            it
-                    .map { TryMonad.value(it) }
-                    .onErrorReturn { TryMonad.error(it) }
-        }
-        val monadToString: (TryMonad<Location>) -> String = {
-            if (it.value != null) {
-                it.value.toString()
-            } else if (it.error != null) {
-                it.error.javaClass.simpleName
-            } else {
-                ""
-            }
-        }
-
-        askLocation
-                .withLatestFrom(locationProvider) { click, locationProvider -> locationProvider }
-                .flatMap {
-                    it.asObservable()
-                            .compose(toTryMonad)
-                            .take(1)
-                }
-                .map(monadToString)
-                .subscribe(RxTextView.text(location))
     }
 
-    private fun buildLocationRequest(): LocationRequest {
-        val locationRequest = LocationRequest()
-        locationRequest.setInterval(10000)
-        locationRequest.setFastestInterval(5000)
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
-        return locationRequest
+    private fun toTryMonad(): (Observable<Location>) -> Observable<TryMonad<Location>> = {
+        it
+                .map { TryMonad.value(it) }
+                .onErrorReturn { TryMonad.error(it) }
+    }
+
+    private fun monadToString(): (TryMonad<Location>) -> String = {
+        if (it.value != null) {
+            it.value.toString()
+        } else if (it.error != null) {
+            it.error.javaClass.simpleName
+        } else {
+            ""
+        }
     }
 }
